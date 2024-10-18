07e662afe14251b5f946c9f9c2b9aa70f9f1bf39
Feaxmutch <feaxmutch@gmail.com>
2024-10-18 17:02:41 +0300

    Now full name in one array

--- Dossier/Program.cs
+++ Dossier/Program.cs
@@ -32,19 +32,19 @@
                 switch (userInput)
                 {
                     case MenuAddDossier:
-                        AddDossier(ref names, ref surnames, ref patronymics, ref posts);
+                        AddDossier(ref names, ref posts);
                         break;
 
                     case MenuWriteDossers:
-                        WriteAllDossiers(names, surnames, patronymics, posts);
+                        WriteAllDossiers(names, posts);
                         break;
 
                     case MenuDeleteDosser:
-                        DeleteDossier(ref names, ref surnames, ref patronymics, ref posts);
+                        DeleteDossier(ref names, ref posts);
                         break;
 
                     case MenuFindDosserBySurname:
-                        FindDossierBySurname(names, surnames, patronymics, posts);
+                        FindDossierBySurname(names, posts);
                         break;
 
                     case CommandExit:
@@ -54,7 +54,7 @@
             }
         }
 
-        static void AddDossier(ref string[] names, ref string[] surnames, ref string[] patronymics, ref string[] posts)
+        static void AddDossier(ref string[] names, ref string[] posts)
         {
             string[] userInputs = new string[4];
 
@@ -62,44 +62,48 @@
             Console.CursorVisible = true;
 
             Console.Write("Введите имя: ");
-            string newName = Console.ReadLine();
+            userInputs[0] = Console.ReadLine();
 
             Console.Write("Введите фамилию: ");
-            string newSurname = Console.ReadLine();
+            userInputs[1] = Console.ReadLine();
 
             Console.Write("Введите отчество: ");
-            string newPatronymic = Console.ReadLine();
+            userInputs[2] = Console.ReadLine();
 
             Console.Write("Введите Должность: ");
-            string newPost = Console.ReadLine();
+            userInputs[3] = Console.ReadLine();
+
+            for (int i = 0; i < userInputs.Length; i++)
+            {
+                if (userInputs[i] == string.Empty)
+                {
+                    userInputs[i] = "Не_указано";
+                }
+            }
 
             Console.CursorVisible = false;
 
-            names = AddElement(names, newName);
-            surnames = AddElement(surnames, newSurname);
-            patronymics = AddElement(patronymics, newPatronymic);
-            posts = AddElement(posts, newPost);
+            names = AddElement(names, $"{userInputs[1]} {userInputs[0]} {userInputs[2]}");
+            posts = AddElement(posts, $"{userInputs[3]}");
 
             Console.WriteLine("Добавленно следющее досье:");
-            WriteDossier(names, surnames, patronymics, posts, names.Length);
+            WriteDossier(names, posts, names.Length);
 
             Console.ReadKey(true);
         }
 
-        static void WriteDossier(string[] names, string[] surnames, string[] patronymics, string[] posts, int dosserNumber)
+        static void WriteDossier(string[] names, string[] posts, int dosserNumber)
         {
-            string name = names[dosserNumber - 1] == string.Empty ? "Не_указано" : names[dosserNumber - 1];
-            string surname = surnames[dosserNumber - 1] == string.Empty ? "Не_указано" : surnames[dosserNumber - 1];
-            string patronymic = patronymics[dosserNumber - 1] == string.Empty ? "Не_указано" : patronymics[dosserNumber - 1];
-            string post = posts[dosserNumber - 1] == string.Empty ? "Не_указано" : posts[dosserNumber - 1];
+            string name = names[dosserNumber - 1];
+            string post = posts[dosserNumber - 1];
 
-            Console.WriteLine($"{dosserNumber}. {surname} {name} {patronymic} - {post}");
+            Console.WriteLine($"{dosserNumber}. {name} - {post}");
         }
 
-        static void DeleteDossier(ref string[] names, ref string[] surnames, ref string[] patronymics, ref string[] posts)
+        static void DeleteDossier(ref string[] names, ref string[] posts)
         {
             Console.Clear();
-            WriteAllDossiers(names, surnames, patronymics, posts);
+            WriteAllDossiers(names, posts);
             Console.Write("\nВведите номер досье, которое хотите удалить ");
             string userInput = Console.ReadLine();
 
@@ -113,8 +117,6 @@
                 else
                 {
                     names = RemoveElement(names, dosserNumber - 1);
-                    surnames = RemoveElement(surnames, dosserNumber - 1);
-                    patronymics = RemoveElement(patronymics, dosserNumber - 1);
                     posts = RemoveElement(posts, dosserNumber - 1);
                 }
             }
@@ -125,10 +127,16 @@
             }
         }
 
-        static void FindDossierBySurname(string[] names, string[] surnames, string[] patronymics, string[] posts)
+        static void FindDossierBySurname(string[] names, string[] posts)
         {
             string userInput = string.Empty;
             bool dossierIsFounded = false;
+            string[] surnames = new string[names.Length];
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                surnames[i] = names[i].Split(' ')[0];
+            }
 
             Console.Clear();
             Console.CursorVisible = true;
@@ -138,9 +146,9 @@
 
             for (int i = 0; i < surnames.Length; i++)
             {
-                if (surnames[i].Contains(userInput.ToLower()))
+                if (surnames[i].ToLower().Contains(userInput.ToLower()))
                 {
-                    WriteDossier(names, surnames, patronymics, posts, i + 1);
+                    WriteDossier(names, posts, i + 1);
                     dossierIsFounded = true;
                 }
             }
@@ -153,13 +161,13 @@
             Console.ReadKey();
         }
 
-        static void WriteAllDossiers(string[] names, string[] surnames, string[] patronymics, string[] posts)
+        static void WriteAllDossiers(string[] names, string[] posts)
         {
             Console.Clear();
 
             for (int i = 1; i <= names.Length; i++)
             {
-                WriteDossier(names, surnames, patronymics, posts, i);
+                WriteDossier(names, posts, i);
             }
 
             Console.ReadKey(true);
